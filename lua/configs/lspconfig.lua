fb49e3225333ca5117820abcd323e0bd533d1328
Juan Felix-Falcon <jfelix@cyberneticks.com>
2024-03-23 11:47:01 -0400

    added go support

---
+++ lua/configs/lspconfig.lua
@@ -0,0 +1,73 @@
+local on_attach = require("nvchad.configs.lspconfig").on_attach
+local on_init = require("nvchad.configs.lspconfig").on_init
+local capabilities = require("nvchad.configs.lspconfig").capabilities
+local util = require "lspconfig/util"
+
+local lspconfig = require "lspconfig"
+local servers = { "html", "cssls" }
+
+-- lsps with default config
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    on_init = on_init,
+    capabilities = capabilities,
+  }
+end
+
+-- python
+lspconfig.pyright.setup {
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+}
+
+-- typescript
+lspconfig.tsserver.setup {
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+}
+
+-- angular
+lspconfig.angularls.setup {
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+}
+
+-- yaml
+lspconfig.yamlls.setup {
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+}
+
+-- helm
+lspconfig.helm_ls.setup {
+  settings = {
+    yamlls = {
+      path = "yaml-language-server",
+    },
+  },
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+}
+
+lspconfig.gopls.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  cmd = { "gopls" },
+  filetypes = { "go", "gomod", "gowork", "gotmpl" },
+  root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+  settings = {
+    gopls = {
+      completeUnimported = true,
+      usePlaceholders = true,
+      analyses = {
+        unusedparams = true,
+      },
+    },
+  },
+}
---
+++ lua/plugins/init.lua
@@ -0,0 +1,62 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    event = { "BufWritePre" },
+    config = function()
+      require "configs.conform"
+    end,
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
+
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "stylua",
+        "html-lsp",
+        "css-lsp",
+        "prettier",
+        "gopls",
+      },
+    },
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+        "typescript",
+      },
+    },
+  },
+
+  {
+    "mfussenegger/nvim-lint",
+    event = { "BufWritePre" },
+    config = function()
+      require "configs.lint"
+    end,
+  },
+  {
+    "fatih/vim-go",
+    ft = "go",
+    config = function() end,
+
+    build = function()
+      vim.cmd [[silent! GoInstallDeps]]
+    end,
+  },
+}
